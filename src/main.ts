cb4f98dec63bddb84cc9138f46340e5cdacb3fea
timofeychesnokv <wladimi482002@gmail.com>
2023-09-04 11:19:50 +0300

    добавлено чтение порта из cfg

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = await app.get(ConfigService);
+  const port = configService.get<number>('application.port');
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
